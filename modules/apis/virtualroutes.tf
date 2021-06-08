ad5172fc65e9c67f331d429b5b235bc7826555c0
Matthew Yacobucci <m.yacobucci@f5.com>
2021-06-07 20:05:42 -0600

    Update command to use the configured prometheus port

--- modules/apis/virtualroutes.tf
+++ modules/apis/virtualroutes.tf
@@ -12,7 +12,7 @@ nginx-meshctl deploy --registry-server docker-registry.nginx.com/nsm
 resource "null_resource" "deploy_nsm" {
 
   provisioner "local-exec" {
-    command = "nginx-meshctl deploy --disable-auto-inject --enabled-namespaces=\"bookinfo,microservice-namespace\" --mtls-mode strict --registry-server docker-registry.nginx.com/nsm --image-tag 1.0.1 --deploy-grafana false --prometheus-address prometheus-service.monitoring:9090"
+    command = "/Users/yacobucci/dev/nginx-plus-kic-nsm-terraform/nginx-meshctl deploy --disable-auto-inject --enabled-namespaces=\"bookinfo,microservice-namespace\" --mtls-mode strict --registry-server docker-registry.nginx.com/nsm --image-tag 1.0.1 --deploy-grafana=false --prometheus-address prometheus-service.monitoring:8080"
    #command = "nginx-meshctl deploy --enabled-namespaces=\"bookinfo,microservice-namespace\" --mtls-mode strict --registry-server docker-registry.nginx.com/nsm --image-tag 1.0.0 --deploy-grafana false"
   }
   depends_on = [kubernetes_namespace.microservice-namespace]
